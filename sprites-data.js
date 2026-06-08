a1e9a77a23b7053148c15518607906680bcd43d6
staticvacant <mewikinn@gmail.com>
2026-06-08 12:09:56 -0500

    Update sprites-data.js

--- sprites-data.js
+++ sprites-data.js
@@ -1,5 +1,12 @@
 // DEVELOPER DATA SHEET: Add new sprites or toggle unreleased states here.
 const baseSprites = [
+    { id: "water_basic", name: "Water Sprite", theme: "Basic", unreleased: false },
+    { id: "water_gold", name: "Gold Water Sprite", theme: "Gold", unreleased: false },
+    { id: "water_candy", name: "Candy Water Sprite", theme: "Candy", unreleased: true },
+    { id: "water_galaxy", name: "Galaxy Water Sprite", theme: "Galaxy", unreleased: true },
+    { id: "water_gem", name: "Gem Water Sprite", theme: "Gem", unreleased: true },
+    { id: "water_holofoil", name: "Holofoil Water Sprite", theme: "Holofoil", unreleased: true },
+    
     { id: "earth_basic", name: "Earth Sprite", theme: "Basic", unreleased: false },
     { id: "earth_gold", name: "Gold Earth Sprite", theme: "Gold", unreleased: false },
     { id: "earth_candy", name: "Candy Earth Sprite", theme: "Candy", unreleased: true },
