d797d8d19b22952e155af2d6de55dd1b7452d3a7
staticvacant <mewikinn@gmail.com>
2026-06-21 15:58:02 -0500

    Update sprites-data.js

--- sprites-data.js
+++ sprites-data.js
@@ -67,7 +67,7 @@ const baseSprites = [
 
 	{ id: "boss_basic", name: "Boss", theme: "Basic", rarity: "Rare", unreleased: true },
 	{ id: "drifter_basic", name: "Drifter", theme: "Basic", rarity: "Rare", unreleased: true },
-	{ id: "fishstick_basic", name: "Fishtick", theme: "Basic", rarity: "Rare", unreleased: true },
+	{ id: "fishstick_basic", name: "Fishstick", theme: "Basic", rarity: "Rare", unreleased: true },
 	{ id: "reaper_galaxy", name: "Reaper", theme: "Galaxy", rarity: "Rare", unreleased: true },
 	{ id: "seven_basic", name: "Seven", theme: "Basic", rarity: "Rare", unreleased: true },
 	{ id: "soccer_basic", name: "Soccer", theme: "Basic", rarity: "Rare", unreleased: true },
